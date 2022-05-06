7da68b88a9613936b1c09f6b671f707d35358bd0
wudhinun <56958411+wudhinun@users.noreply.github.com>
2022-05-06 05:00:10 -0400

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
+//app.set('view engine', 'handlebars');
+app.engine('handlebars', exphbs.engine());
 app.set('port', config.port);
 
 app.use('/', express.static('public'))
